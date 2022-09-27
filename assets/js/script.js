1917a078f43e4d3232228cefd1034b94a662daad
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2022-09-27 16:10:38 -0400

    Mobile-Nav-Complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,33 @@ navClose.addEventListener("click", () => {
 
 });
 
+//Remove menu on click################
+const navLink = document.querySelectorAll('.nav-link');
+
+linkAction = () => {
+    navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach((link) => link.addEventListener('click', linkAction));
+
+//Make Active Link #################
+const sections = document.querySelectorAll("section[id");
+
+activeScroll = () => {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach((section) => {
+        const sectionHeight = section.offsetHeight;
+        const sectionTop = section.offsetTop - 50;
+        sectionId = section.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.main-nav a[href*=' + sectionId + ']').classList.add('active-link');
+
+        } else {
+            document.querySelector('.main-nav a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+
+    });
+} 
+window.addEventListener('scroll', activeScroll);
